a732adf47676ad8b0f85dc7662486eea5c77dad3
Albert Lai <albertlai631@outlook.com>
2020-01-03 13:53:36 +0000

    Initial code for StoreMenu and GameWorld

--- Gungeon/GameWorld.java
+++ Gungeon/GameWorld.java
@@ -1,13 +1,18 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * GameWorld is the world where the game takes place. It holds the player, elements of the * game, 2D array of actors, and game data. 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * 
+ * @author Albert Lai
+ * @version January 2020
  */
 public class GameWorld extends World
 {
+		private Actor [][];
+		private Player player;
+		private int curLevel;
+		
 
     /**
      * Constructor for objects of class MyWorld.
@@ -18,4 +23,24 @@ public class GameWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
     }
+
+		private void gameOver(){
+
+		}
+
+		public void act(){
+
+		}
+
+		private void loadTextFile(){
+
+		}
+
+		public void pause(){
+
+		}
+
+		public void play(){
+			
+		}
 }
--- Gungeon/StoreMenu.java
+++ Gungeon/StoreMenu.java
@@ -1,13 +1,18 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class StoreMenu here.
+ * StoreMenu is the store that exists in the game. The user can open the Store anytime
+ * when playing the game and choose to buy items/powerups or equip themselves with 
+ * an item. 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Albert Lai
+ * @version January 2020
  */
 public class StoreMenu extends Actor
 {
+
+			
+
     /**
      * Act - do whatever the StoreMenu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +20,13 @@ public class StoreMenu extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+    }
+
+		private boolean purchase(){
+
+		}    
+
+		private boolean equip(){
+
+		}
 }
