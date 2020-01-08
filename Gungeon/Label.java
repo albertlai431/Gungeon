9c896fe952dc2710e70f45b3725e21e834a50e9c
Alex-Li107 <58983570+Alex-Li107@users.noreply.github.com>
2020-01-08 08:38:48 -0500

    Update Label.java

--- Gungeon/Label.java
+++ Gungeon/Label.java
@@ -1,19 +1,82 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Label here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * A Label class that allows you to display a textual value on screen.
+ * <p>
+ * The Label is an actor, so you will need to create it, and then add it to the world
+ * in Greenfoot.
+ *
+ * @author Alex Li
+ * @version 1.2.1
  */
 public class Label extends Actor
 {
+    //instance variables
+    private String text;
+    private int fontSize;
+    private boolean isTransparent;
+    //default colours
+    private Color textColour = Color.BLACK;
+    private Color backgroundColor;
+    private Font font;
+    private GreenfootImage img;
+    private TextSizeFinder finder = new TextSizeFinder();
     /**
-     * Act - do whatever the Label wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Creates a default label with customizable font size and text. The text will have a black outline on a white background
+     * 
+     * @param text             The text that you want to label to have
+     * @param fontSize          The desired fontSize
+     * @param isTransparent     True if the background is transparent
      */
-    public void act() 
+    public Label(String text, int fontSize, boolean isTransparent)
     {
-        // Add your action code here.
-    }    
+        this.text = text;
+        this.fontSize = fontSize;
+        this.isTransparent = isTransparent;
+        font = new Font ("calibri", false, false, fontSize);
+        textColour = Color.BLACK;
+        if(isTransparent) backgroundColor = new Color(0,0,0,0);
+        else backgroundColor = new Color(0,0,0);
+        //draws the image
+        updateImage();
+    }
+    public Label(String text, int fontSize, int txtR, int txtB, int txtG, boolean isTransparent)
+    {
+        this.text = text;
+        this.fontSize = fontSize;
+        isTransparent = false;
+        font = new Font ("calibri", false, false, fontSize);
+        textColour = new Color(txtR,txtB,txtG);
+        if(isTransparent) backgroundColor = new Color(0,0,0,0);
+        else backgroundColor = new Color(0,0,0);
+        //draws the image
+        updateImage();
+    }
+    //must have a background
+    public Label(String text, int fontSize, int txtR, int txtB, int txtG, int backR, int backB, int backG)
+    {
+        this.text = text;
+        this.fontSize = fontSize;
+        isTransparent = false;
+        font = new Font ("calibri", false, false, fontSize);
+        textColour = new Color(txtR,txtB,txtG);
+        backgroundColor = new Color(backR, backB, backG);
+        //draws the image
+        updateImage();
+    }
+    /**
+     * Updates the image on screen to show the current text with its specified font size and colour.
+     */
+    private void updateImage()
+    {
+        img = new GreenfootImage(finder.getTextWidth(text,fontSize), finder.getTextHeight(text,fontSize));
+        img.setFont(font);
+        if(!isTransparent){
+            img.setColor(backgroundColor);
+            img.fill();
+        }
+        img.setColor(textColour);
+        img.drawString(text,0,img.getHeight() - (img.getHeight()/3));
+        setImage(img);
+    }
 }
