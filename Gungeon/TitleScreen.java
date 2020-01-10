476f759eb9dbbd7de4e1c37c80ded4fd75f837a5
Alex-Li107 <58983570+Alex-Li107@users.noreply.github.com>
2020-01-09 21:08:14 -0500

    Update TitleScreen.java

--- Gungeon/TitleScreen.java
+++ Gungeon/TitleScreen.java
@@ -8,7 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-
+    private Button play = new Button("PLAY",35);
+    private Button options = new Button("OPTIONS",35);
+    private Button load = new Button("LOAD",35);
+    private MouseInfo mouse = Greenfoot.getMouseInfo();
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -16,6 +19,23 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(960, 640, 1); 
+        addObject(play,35,500);
+        addObject(load,40,530);
+        addObject(options,63,560);
+    }
+    public void act(){
+        if(Greenfoot.mouseClicked(play)){
+            GameWorld gameWorld = new GameWorld(/*bool*/);
+            Greenfoot.setWorld(gameWorld);
+        }
+        else if(Greenfoot.mouseClicked(load)){
+            GameWorld gameWorld = new GameWorld(/*bool*/);
+            Greenfoot.setWorld(gameWorld);
+        }
+        else if(Greenfoot.mouseClicked(options)){
+            Options optionWorld = new Options();
+            Greenfoot.setWorld(optionWorld);
+        }
     }
 }
