a6d9d4cbc01d8e65df53fef230fd748eb92e62f3
xiestar0310 <59263550+xiestar0310@users.noreply.github.com>
2020-01-09 21:10:40 -0500

    Update Player.java

--- Gungeon/Player.java
+++ Gungeon/Player.java
@@ -1,19 +1,40 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+ 
 /**
- * Write a description of class Player here.
- * 
- * @author (your name) 
+* Write a description of class Player here.
+*
+ * @author (your name)
  * @version (a version number or a date)
- */
+*/
 public class Player extends Actor
 {
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
-        // Add your action code here.
-    }    
+       if(Greenfoot.isKeyDown("a"))//runs if "a" is pressed and the player is past the starting location
+            {
+                imageLeft();
+            }
+            else if(Greenfoot.isKeyDown("d"))//runs if "d" is pressed
+            {
+                imageRight();
+            }
+            else if(Greenfoot.isKeyDown("w"))//runs if "d" is pressed
+            {
+                imageUp();
+            }
+            else if(Greenfoot.isKeyDown("s"))//runs if "d" is pressed
+            {
+                imageDown();
+            }
+    }   
+    
+    public void imageLeft()
+    {
+        GreenfootImage img = getImage();
+       
+    }
 }
