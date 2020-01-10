e2ab34276220dcdf16543d0e02df9d62025d1265
xiestar0310 <59263550+xiestar0310@users.noreply.github.com>
2020-01-09 19:18:42 -0500

    Update AnimationInterface.java

--- Gungeon/AnimationInterface.java
+++ Gungeon/AnimationInterface.java
@@ -1,10 +1,17 @@
 /**
- * Write a description of class AnimationInterface here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The animation interface is designed to be implemented by enemies or characters requiring movement in all 4 directions 
+ * of freedom. The interface ensures all enemies and characters follow through with this animation as required. 
+ *
+ * @author Star Xie 
+ * @version January 2020
  */
-public interface AnimationInterface  
+public interface Animation 
 {
+    public void animateMovementUp();
     
+    public void animateMovementDown();
+    
+    public void animateMovementRight();
+    
+    public void animateMovementLeft();
 }
