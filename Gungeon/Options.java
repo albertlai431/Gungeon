33ddde5e6dd25900207793285e511b630eb4163d
Alex-Li107 <58983570+Alex-Li107@users.noreply.github.com>
2020-01-12 21:44:17 -0500

    Update Options.java
    Added Albert's tutorial imgs

--- Gungeon/Options.java
+++ Gungeon/Options.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class Options here.
@@ -25,6 +26,8 @@ public class Options extends World
     private Label text5 = new Label("\"E\" to swap guns",16,true);
     private Label text6 = new Label("\"Esc\" pasues the game",16,true);
     private Button back = new Button("BACK",35);
+    private Button next = new Button("NEXT",35);
+    private int tutorialCount = 0;
     /**
      * Constructor for objects of class Options.
      * 
@@ -43,7 +46,8 @@ public class Options extends World
         addObject(text4,567,515);
         addObject(text5,556,565);
         addObject(text6,575,620);
-        addObject(back,915,620);
+        addObject(back,50,620);
+        addObject(next,911,620);
         getBackground().drawImage(wasd,300,125);
         getBackground().drawImage(mouse,300,300);
         getBackground().drawImage(shop,300,450);
@@ -53,8 +57,61 @@ public class Options extends World
     }
     public void act(){
         if(Greenfoot.mouseClicked(back)){
-            TitleScreen titleWorld = new TitleScreen();
-            Greenfoot.setWorld(titleWorld);
+            if(tutorialCount==0){
+                TitleScreen titleWorld = new TitleScreen();
+                Greenfoot.setWorld(titleWorld);
+            }
+            else if(tutorialCount == 1){
+                List objects = getObjects(null);
+                removeObjects(objects);
+                getBackground().fill();
+                addObject(title,480,50);
+                addObject(subTitle1,480,100);
+                addObject(subTitle2,480,250);
+                addObject(subTitle3,480,420);
+                addObject(text1,600,175);
+                addObject(text2,717,330);
+                addObject(text3,590,462);
+                addObject(text4,567,515);
+                addObject(text5,556,565);
+                addObject(text6,575,620);
+                addObject(back,50,620);
+                addObject(next,911,620);
+                getBackground().drawImage(wasd,300,125);
+                getBackground().drawImage(mouse,300,300);
+                getBackground().drawImage(shop,300,450);
+                getBackground().drawImage(reload,300,500);
+                getBackground().drawImage(switchGun,300,550);
+                getBackground().drawImage(Pause,300,600);
+                tutorialCount--;
+            }
+            else if(tutorialCount == 2){
+                tutorialCount--;
+                List objects = getObjects(null);
+                removeObjects(objects);
+                getBackground().fill();
+                addObject(next,911,620);
+                addObject(back,50,620);
+                getBackground().drawImage(new GreenfootImage("tutorial"+tutorialCount+".png"),0,0);
+            }
+        }
+        if(Greenfoot.mouseClicked(next)){
+            tutorialCount++;
+            if(tutorialCount == 1){
+                List objects = getObjects(null);
+                removeObjects(objects);
+                getBackground().fill();
+                addObject(next,911,620);
+                addObject(back,50,620);
+                getBackground().drawImage(new GreenfootImage("tutorial"+tutorialCount+".png"),0,0);
+            }
+            else if(tutorialCount == 2){
+                List objects = getObjects(null);
+                removeObjects(objects);
+                getBackground().fill();
+                addObject(back,50,620);
+                getBackground().drawImage(new GreenfootImage("tutorial"+tutorialCount+".png"),0,0);
+            }
         }
     }
 }
