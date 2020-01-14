4c54cf9e9086cfb1a93e583e0de712728bbc76b2
Shock2277 <58834795+Shock2277@users.noreply.github.com>
2020-01-14 17:16:31 -0500

    Update SniperEnemy.java

--- Gungeon/SniperEnemy.java
+++ Gungeon/SniperEnemy.java
@@ -8,65 +8,90 @@ import java.util.ArrayList;
  */
 public class SniperEnemy extends Enemy
 {
-    private static int imageX;
-    private static int imageY;
-    public SniperEnemy(int health, int bulletWidth)
-    {
-        animationImage = new GreenfootImage("bulletEnemyDown0.png");
-        //animationImage.scale(imageX, imageY);
-        setImage(animationImage);        
-        healthPoints = health;     
-        this.bulletWidth = bulletWidth;
-        fireRate = 60;    
+    private static GreenfootImage[] rightMvt = new GreenfootImage[5];
+    private static GreenfootImage[] leftMvt = new GreenfootImage[5];
+    private static GreenfootImage[] upMvt = new GreenfootImage[8];
+    private static GreenfootImage[] downMvt = new GreenfootImage[8];
+    private static boolean createdImages = false;
+    private int frameRate = 8;
+    private int imageNumber = 0;    
+    public SniperEnemy()
+    {      
+        healthPoints = 500;     
+        this.bulletWidth = 15;
+        fireRate = 90;    
     }
 
+    protected void addedToWorld(World world) 
+    {
+        createImages();
+    }
+    
     public void act() 
     {
         moveTowardsPlayer();       
     }    
- 
     
     public void attack()
     {
-        //getWorld().addObject(new SniperBullet(player.getX(), player.getY(), 1, 30), getX(), getY());          
+        getWorld().addObject(new SniperBullet(player.getX(), player.getY(), 1, 15), getX(), getY());          
     }
-    
-    public void animate(String direction)
-    {
-        int totalFrames = 0;
-        String imageName = new String(direction);
-        if(direction.equals("Left") || direction.equals("Right"))
-        {
-            totalFrames = 4;
-            imageName = new String("Left");
-        }
-        else
-        {
-            totalFrames = 7;
-        }
-        for(int i = 0; i < totalFrames; i++)
-        {
-            animationImage = new GreenfootImage("sniperEnemy" + imageName + i + ".png");
-            animationImage.scale(16, 28);
-            animationImage.mirrorHorizontally();
-            setImage(animationImage);
-            move(1);
-        }
-    }
-    
+      
     public void animateMovementUp()
     {
+        if(animationCount%frameRate == 0)
+        {
+            imageNumber = (imageNumber + 1)% (upMvt.length);
+            setImage(upMvt[imageNumber]);
+        }        
     }
     
     public void animateMovementDown()
     {
+        if(animationCount%frameRate == 0)
+        {
+            imageNumber = (imageNumber + 1)% (downMvt.length);
+            setImage(downMvt[imageNumber]);
+        }        
     }
     
     public void animateMovementRight()
     {
+        if(animationCount%frameRate == 0)
+        {
+            imageNumber = (imageNumber + 1)% (rightMvt.length);
+            setImage(rightMvt[imageNumber]);
+        }
     }
     
     public void animateMovementLeft()
     {
+        if(animationCount%frameRate == 0)
+        {
+            imageNumber = (imageNumber + 1)% (leftMvt.length);
+            setImage(leftMvt[imageNumber]);
+        }
+    }
+    
+    public static void createImages()
+    {
+        if(!createdImages)
+        {
+            createdImages = true;
+            for(int i=0; i<rightMvt.length; i++)
+            {
+                rightMvt[i] = new GreenfootImage("sniperEnemyRight"+i+".png");
+                leftMvt[i] = new GreenfootImage("sniperEnemyRight"+i+".png");
+            }
+            for(int i=0; i<leftMvt.length; i++)
+            {
+                leftMvt[i].mirrorHorizontally();
+            }
+            for(int i=0; i<upMvt.length; i++)
+            {
+                upMvt[i] = new GreenfootImage("sniperEnemyUp"+i+".png");
+                downMvt[i] = new GreenfootImage("sniperEnemyDown"+i+".png");
+            }
+        }
     }
 }
