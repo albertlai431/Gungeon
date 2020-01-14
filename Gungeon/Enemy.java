1b1dc624216749802c0b6c078d48e4027df06e0e
Shock2277 <58834795+Shock2277@users.noreply.github.com>
2020-01-13 20:51:56 -0500

    Update Enemy.java

--- Gungeon/Enemy.java
+++ Gungeon/Enemy.java
@@ -1,4 +1,5 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+import greenfoot.*; 
 import java.util.ArrayList;
 /**
  * Write a description of class Enemy here.
@@ -6,10 +7,10 @@ import java.util.ArrayList;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Enemy extends Actor
+public abstract class Enemy extends Actor implements AnimationInterface
 {
-    protected static int tileSizeX;
-    protected static int tileSizeY;
+    protected static int tileSizeX = 20;
+    protected static int tileSizeY = 20;
     protected int currentX;
     protected int currentY;
     protected int playerX;
@@ -18,31 +19,39 @@ public abstract class Enemy extends Actor
     protected int healthPoints;
     protected int bulletSpeed;
     protected int damage = 1;
+    protected int fireRate;
+    protected GreenfootImage animationImage;
     protected ArrayList<Player> foundPlayers;
     protected Actor player;
 
     protected abstract void attack();
-    
-    public void getHit(int damage)
+
+    public void getDamaged(int damage)
     {
         healthPoints -= damage;
         if(healthPoints <= 0)
-        {
+        {           
             die();
         }
     }
-    
+
     protected void moveTowardsPlayer()
     {
         int[] closestTileCoordinates = findClosestAdjacentTileTowardsPlayer();
         turnTowards(closestTileCoordinates[0], closestTileCoordinates[1]);
-        while(getX() != closestTileCoordinates[0] && getY() != closestTileCoordinates[1])
+        animate(getDirection(getRotation()));        
+        while(checkLineOfSight(bulletWidth, getPointDistance(getX(), getY(), player.getX(), player.getY())) == true)
         {
-            move(1);
-            while(checkLineOfSight(bulletWidth, getPointDistance(getX(), getY(), player.getX(), player.getY())) == true)
+            int counter = fireRate;
+            if(fireRate == counter)
             {
                 attack();
-            } 
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
         }
     }
 
@@ -54,7 +63,7 @@ public abstract class Enemy extends Actor
 
     private boolean checkForObstacle(int x, int y)
     {
-        ArrayList obstacleList = new ArrayList<Player>(getWorld().getObjectsAt(x, y, Player.class));
+        ArrayList obstacleList = new ArrayList<Player>(getWorld().getObjectsAt(x, y, Obstacles.class));
         if(obstacleList.isEmpty())
         {
             return false;
@@ -64,6 +73,8 @@ public abstract class Enemy extends Actor
 
     private int[] findClosestAdjacentTileTowardsPlayer()
     {
+        foundPlayers = new ArrayList<Player>(getWorld().getObjects(Player.class));
+        player = foundPlayers.get(0);        
         currentX = getX();
         currentY = getY();        
         playerX = player.getX();
@@ -72,15 +83,15 @@ public abstract class Enemy extends Actor
         int coordinateXStart = currentX - tileSizeX;
         int coordinateYStart = currentY - tileSizeY;
         int closestTileX = 0;
-        int closestTileY = 0;
+        int closestTileY = 0;        
         for(int x = 0; x < 3; x ++)
         {
             for(int y = 0; y < 3; y ++)
             {
-                if(x == 2 && y == 2){}         
+                if(x == 1 && y == 1){}         
                 else
                 {
-                    int[] tileCoordinates = new int[]{coordinateXStart + x * tileSizeX, coordinateYStart + y * tileSizeY};
+                    int[] tileCoordinates = new int[]{coordinateXStart + (x * tileSizeX), coordinateYStart + (y * tileSizeY)};                    
                     double distance = getPointDistance(tileCoordinates[0], tileCoordinates[1], playerX, playerY);
                     if(checkForObstacle(tileCoordinates[0], tileCoordinates[1])){}                    
                     else
@@ -94,7 +105,7 @@ public abstract class Enemy extends Actor
                     }
                 }
             }
-        }
+        }      
         int[] closestTileCoordinates = new int[]{closestTileX, closestTileY};
         return closestTileCoordinates;
     }
@@ -102,12 +113,38 @@ public abstract class Enemy extends Actor
     private boolean checkLineOfSight(int bulletWidth, double bulletLength)
     {
         LineOfSightRect line = new LineOfSightRect(bulletWidth, (int)getPointDistance(getX(), getY(), player.getX(), player.getY()) + 5);
+        getWorld().addObject(line, 2, 2);
         return line.playerVisible(getRotation(), getX(), getY(), player.getX(), player.getY());
     }
-    
+
     protected void die()
     {
-        //play animation code
+        getWorld().updateScore();
         getWorld().removeObject(this);
     }
+
+    protected abstract void animate(String direction);
+
+    protected String getDirection(int rotation)
+    {
+        if(rotation >= 225 && rotation < 315)
+        {
+            return "Up";
+        }
+        else if((rotation >= 315 && rotation < 360) || (rotation >= 0 && rotation < 45))
+        {
+            return "Right";
+        }
+        else if(rotation >= 45 && rotation < 135)
+        {
+            return "Down";
+        }  
+        else if(rotation >= 135 && rotation < 225)
+        {
+            return "Left";
+        }        
+        return "";
+    }
+    
+    
 }
