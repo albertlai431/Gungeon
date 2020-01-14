fda215e94769cc149fccdbce0777c06fa25adbb9
Alex-Li107 <58983570+Alex-Li107@users.noreply.github.com>
2020-01-14 09:05:01 -0500

    Update ItemInfo.java

--- Gungeon/ItemInfo.java
+++ Gungeon/ItemInfo.java
@@ -13,46 +13,47 @@ public class ItemInfo extends Actor
     private GreenfootImage[] guns = new GreenfootImage[3];
     private GreenfootImage moneyIcon = new GreenfootImage("Money.png");
     private GreenfootImage killsIcon = new GreenfootImage("KillsIcon.png");
-    private int currentGun, totalAmmo, currentAmmo, money = 0, kills = 0,score = 0;
+    private int currentGun, totalAmmo, currentAmmo, money, kills = 0,score = 0;
     private Label ammoLabel, moneyLabel, killsLabel,scoreLabel;
     /**
      * @param currentGun            0 = pistol, 1 = rifle, 2 = shotgun
      * @param totalAmmo             The total ammo of a specific type the player has. If the current gun is the pistol, totalAmmo = -1 
-     * @param currentAmmo           The amount of bullets in gun's magazine  
+     * @param currentAmmo           The amount of bullets in gun's magazine 
+     * @param money                 The player's starting money
      */
-    public ItemInfo(int currentGun, int totalAmmo, int currentAmmo){
+    public ItemInfo(int currentGun, int totalAmmo, int currentAmmo,int money){
         GameWorld world = (GameWorld) getWorld();
         myImg= new GreenfootImage(274,79);
-        myImg.setColor(borderColor);
-        myImg.fill();
-        myImg.setColor(backgroundColour);
-        myImg.fillRect(2,2,270,75);
-        myImg.setColor(borderColor);
-        myImg.drawRect(0,0,110,79);
-        setImage(myImg);
+        drawBoard();
         guns[0] = new GreenfootImage("Pistol.png");
         guns[1] = new GreenfootImage("Rifle.png");
         guns[2] = new GreenfootImage("Shotgun.png");
         this.currentGun = currentGun;
         this.totalAmmo = totalAmmo;
         this.currentAmmo = currentAmmo;
-        updateAmmoLabel();
-        myImg.drawImage(moneyIcon,120,5);
-        myImg.drawImage(killsIcon,120,25);
-        moneyLabel = new Label("0",18,true);
+        this.money = money;
+        moneyLabel = new Label(Integer.toString(money),18,true);
         killsLabel = new Label("0",18,true);
         scoreLabel = new Label("0",18,true);
         drawGun();
     }
     public void addedToWorld(World world){
-        if(currentGun == 0) world.addObject(ammoLabel,758,628);
-        else if(currentGun == 1) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);
-        else if(currentGun == 2) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);
+        if(currentGun == 0) {
+            ammoLabel = new Label(Integer.toString(currentAmmo)+"/infinite",16,true);
+            world.addObject(ammoLabel,758,628);
+        }
+        else{
+            ammoLabel = new Label(Integer.toString(currentAmmo)+"/"+Integer.toString(totalAmmo),16,true);
+            if(currentGun == 1) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);
+            else if(currentGun == 2) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);
+        }
         world.addObject(moneyLabel, 837 + (moneyLabel.getLength()/2), 577);
         world.addObject(killsLabel, 837 + (killsLabel.getLength()/2), 599);
         world.addObject(scoreLabel, 813 + (killsLabel.getLength()/2), 622);
     } 
     private void drawGun(){
+        myImg.clear();
+        drawBoard();
         if(currentGun == 0){
             guns[0].scale(54,33);
             myImg.drawImage(guns[currentGun],10,5);
@@ -68,9 +69,9 @@ public class ItemInfo extends Actor
     }   
     private void updateAmmoLabel(){
         GameWorld world = (GameWorld) getWorld(); 
-        if(currentGun == 0) ammoLabel = new Label(Integer.toString(currentAmmo)+"/infinite",16,true);           
-        else ammoLabel = new Label(Integer.toString(currentAmmo)+"/"+Integer.toString(totalAmmo),16,true);
         if(getWorld()!=null){
+            if(currentGun == 0) ammoLabel.updateText(Integer.toString(currentAmmo)+"/infinite");           
+            else ammoLabel = new Label(Integer.toString(currentAmmo)+"/"+Integer.toString(totalAmmo),16,true);
             if(currentGun == 0) world.addObject(ammoLabel,758,628);
             else if(currentGun == 1) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);
             else if(currentGun == 2) world.addObject(ammoLabel,790-(ammoLabel.getLength()/2),628);            
@@ -87,6 +88,17 @@ public class ItemInfo extends Actor
     private void updateScoreLabel(){
         scoreLabel = new Label(Integer.toString(score),18,true);
         if(getWorld()!=null) getWorld().addObject(scoreLabel, 813 + (scoreLabel.getLength()/2), 622);
+    }
+    private void drawBoard(){
+        myImg.setColor(borderColor);
+        myImg.fill();
+        myImg.setColor(backgroundColour);
+        myImg.fillRect(2,2,270,75);
+        myImg.setColor(borderColor);
+        myImg.drawRect(0,0,110,79);
+        setImage(myImg);
+        myImg.drawImage(moneyIcon,120,5);
+        myImg.drawImage(killsIcon,120,25);
     }
     /**
      * Call this method when the user switches guns
