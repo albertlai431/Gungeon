a82429fb659f62340bd053b0cd770030c1f47b7c
xiestar0310 <59263550+xiestar0310@users.noreply.github.com>
2020-01-16 22:07:18 -0500

    Comments

--- Gungeon/ShotgunBullet.java
+++ Gungeon/ShotgunBullet.java
@@ -1,25 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
- 
+
 /**
-* Bullet- a subclass of Ammunition where bullets are made. Bullets follow a
-* straight line path from the angle they are shot at and will do a constant
-* damage to the HP of the Building/Vehicle that it hits.
-*
- * @author Star Xie, Albert Lai, Clarence Chau
-* @version November 2019
-*/
+ * ShotgunBullet- a subclass of Ammunition where bullets are made. Bullets follow a
+ * straight line path from the angle they are shot at and will do a constant
+ * damage to the HP of the player/enemy that it hits.
+ *
+ * @author Star Xie, Clarence Chau
+ * @version January 2020
+ */
 public class ShotgunBullet extends Ammunition
 {
-    //declare greenfoot sounds
+    //declare greenfoot images and sounds
     private GreenfootImage bullet = new GreenfootImage("shotgunBullet.png");
- 
+
     /**
-     * Constructor - creates a Bullet, sets the team, targets an Actor, and specifies damage dealt
+     * Constructor - creates a ShotgunBullet, sets the team, targets specified coordinates, and specifies damage dealt
      *
-     * @param red               specifies the team (true for red, false for blue)
-     * @param actor             the specific object that is being targetted
+     * @param xCoord            the targetted X coordinate
+     * @param yCoord            the targetted Y coordinate
      * @param damage            specifies the damage taken for each hit
+     * @param speed             speed the bullet travels at
+     * @param isEnemy           true if the bullet is firing from an enemy, otherwise false
      */
     public ShotgunBullet(int x, int y, int damage, int speed, boolean isEnemy)
     {
@@ -28,12 +30,18 @@ public class ShotgunBullet extends Ammunition
         //hit.setVolume(50);
         //shoot.setVolume(75);
     } 
-   
+
+    /**
+     * reloadAmmo - Sets the ammunition to it's full amount. To be implemented in subclasses
+     */
     public void reloadAmmo()
     {
         ammo = 5;
     }
-   
+
+    /**
+     * checkAmmo - Returns the specific ammo number. To be implemented in subclasses
+     */
     public int checkAmmo()
     {
         return ammo;
