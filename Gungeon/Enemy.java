8dcf969da30bcf0820ffe0657b024baaacb528d2
Shock2277 <58834795+Shock2277@users.noreply.github.com>
2020-01-16 21:20:27 -0500

    Update Enemy.java

--- Gungeon/Enemy.java
+++ Gungeon/Enemy.java
@@ -1,52 +1,62 @@
 import greenfoot.*; 
 import java.util.ArrayList;
 /**
- * Write a description of class Enemy here.
+ * Abstract class for all enemies in the game that target and attack the player
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Combined(Henry Ma), Debugging(Star, Albert), Pathfinding(Albert), Animations(Star)
+ * @version January 16, 2020
  */
 public abstract class Enemy extends Actor implements AnimationInterface
 {
-    protected static int tileSizeX = 32;
-    protected static int tileSizeY = 32;
-    protected int currentX;
-    protected int currentY;
+    //Initialize variables and objects
+    //Coordinates used for methods
     protected int playerX;
     protected int playerY;
-    protected int bulletWidth;
+    //Health of enemy(this)
     protected int healthPoints;
-    protected int bulletSpeed;
-    protected int quadrant;
+    //Used for attacks
     protected int counter=0;
-    protected int damage = 1;
     protected int fireRate;
+    //Used for animation
     protected long animationCount = 0;
     protected int movementCounter = 30;
-    protected GreenfootImage animationImage;
+    //Used to find player
     protected ArrayList<Player> foundPlayers;
     protected Actor player;
+    //Used for animation
     protected boolean animated = false;
+    //Used to call methods in the world
     protected int scoreBoost;
-    protected int moneyBoost;
-    
+    protected int moneyBoost;    
+    /**
+     * Call to inflict damage, checks for death as well
+     * 
+     * @param damage    Amount of damage this object takes
+     */
     public void getDamaged(int damage)
     {
+        //Reduce health
         healthPoints -= damage;
+        //If below or at zero hp die
         if(healthPoints <= 0)
         {           
             die();
         }
+        //If touching player die and harm player
         if(getWorld()!=null && this.isTouching(Player.class)) 
         {
             getWorld().getObjects(Player.class).get(0).loseOneHeart();
             die(); 
         }
-    }
-
+    }    
+    /**
+     * Move towards player and checks conditions for attacking and moving
+     */
     protected void moveTowardsPlayer()
     {
+        //Find closeset tile to player
         int[] closestTileCoordinates = findClosestAdjacentTileTowardsPlayer();
+        //Checks for conditions if attacking is possible
         if(checkLineOfSight() == false || animated==false)
         {
             animated=true;
@@ -69,32 +79,61 @@ public abstract class Enemy extends Actor implements AnimationInterface
             }
         }        
     }
-
+    /**
+     * All enemies must contain an attack method(method/pattern on how they attack)
+     */
     protected abstract void attack();
-
+    /**
+     * Get the distance between two objects
+     * 
+     * @param enemyX    X-coordinate of first object
+     * @param enemyY    Y-coordinate of first object
+     * @param playerX   X-coordinate of second object
+     * @param playerY   Y-coordinate of second object
+     * 
+     * @return double   Returns the distance between the two coordinates as a double
+     */
     private double getPointDistance(int enemyX, int enemyY, int playerX, int playerY)
     {
         double distance = Math.sqrt(Math.pow(playerX - enemyX, 2) + Math.pow(playerY - enemyY ,2));     
         return distance;
     }
-
+    /**
+     * Checks for objects(Walls) at the coordinates given in the parameters
+     * 
+     * @param x     X-coordinate of the spot to check
+     * @param y     Y-coordinate of the spot to check
+     * 
+     * @return boolean  Returns true if object is at coordinates false otherwise
+     */
     private boolean checkForWall(int x, int y)
     {
+        //Get objects of Walls.class at x and y
         ArrayList <Walls> obstacleList = (ArrayList) (getWorld().getObjectsAt(x, y, Walls.class));
+        //If walls exist return false
         if(obstacleList.isEmpty())
         {
             return false;
         }
         return true;
     }
-
+    /**
+     * Gets the coordinates of a tile that is closest to player
+     * 
+     * @return  int[]   Returns the coordinates in an array of the tile closest to player
+     */
     public int[] findClosestAdjacentTileTowardsPlayer()
     {        
         foundPlayers = new ArrayList<Player>(getWorld().getObjects(Player.class));
         player = foundPlayers.get(0); 
+        //Call pathfinding class to get tile coordinates
         return Pathfinding.nextCoord(getX(),getY(), (GameWorld) getWorld(), (Player) player);
     }
-
+    /**
+     * Checks whether at the current position of this object if it can hit the player with bullets or not
+     * 
+     * @return boolean  Returns true if there is line of sight, false otherwise
+     */
     private boolean checkLineOfSight()
     {
         //find dx, dy
@@ -129,40 +168,20 @@ public abstract class Enemy extends Actor implements AnimationInterface
         }
         return true;
     }
-    /*
-    private int getBulletRotation()
-    {
-        int Xdisplacement = Math.abs(currentX-playerX);
-        int Ydisplacement = Math.abs(currentY-playerY);
-        int theta = 0;
-        if(Ydisplacement != 0)
-        {
-            theta = (int) Math.atan(Xdisplacement/Ydisplacement);
-        }
-        else theta = 0;
-
-        if(currentY-playerY>=0)
-        {
-            if(currentX-playerX>=0) theta = 180 + theta;
-            else theta = 180 - theta;
-        }
-        else
-        {
-            if(currentX-playerX>=0)theta = 360-theta;
-            else theta = theta;
-        }
-
-        return theta;
-    }
-    */
-
+    /**
+     * Removes object from the world properly
+     */
     protected void die()
     {
         GameWorld world = (GameWorld) getWorld();
         world.updateScore(scoreBoost,moneyBoost);
         world.removeObject(this);
     }
-
+    /**
+     * Animates the object, checks the coordinates given in the parameters to determine the direction of the animation
+     * 
+     * @param closestTileCoordinates    The coordinates of the object to animate/face towards
+     */
     protected void animate(int[] closestTileCoordinates)
     {
         if(closestTileCoordinates[0] > getX() && closestTileCoordinates[1] == getY())
@@ -232,4 +251,3 @@ public abstract class Enemy extends Actor implements AnimationInterface
         }         
     }
 }    
-
