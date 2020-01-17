49620f68c578c3dca0e5cf1e969d54e5a3317125
xiestar0310 <59263550+xiestar0310@users.noreply.github.com>
2020-01-16 22:00:46 -0500

    Commented class

--- Gungeon/Cursor.java
+++ Gungeon/Cursor.java
@@ -1,29 +1,34 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Crusor here.
+ * Cursor- a class meant to act as an image for the mouse to have the appearance as a targetting device.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Star Xie
+ * @version January 2020
  */
 public class Cursor extends Actor
 {
+    //Declare variables
     private int XCoord;
     private int YCoord;
+    /**
+     * Constructor - creates a Cursor with a scaled image
+     */
     public Cursor()
     {
-        GreenfootImage image = new GreenfootImage("cursor.png");
-        setImage(image);
+        GreenfootImage image = new GreenfootImage("crusor.png");
+        //Scales and sets image
         image.scale(image.getWidth()*150/100, image.getHeight()*150/100);
-        
-        
+        setImage(image);
     }
+    
     /**
      * Act - do whatever the Crusor wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        //Checks if the mouse is currently in the world, sets location
         if(Greenfoot.getMouseInfo() != null)
         {
             XCoord = Greenfoot.getMouseInfo().getX();
