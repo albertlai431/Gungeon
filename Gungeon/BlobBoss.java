d2f6bc63091ece38a5db9de43b5596244454572a
Shock2277 <58834795+Shock2277@users.noreply.github.com>
2020-01-16 23:11:25 -0500

    Update BlobBoss.java

--- Gungeon/BlobBoss.java
+++ Gungeon/BlobBoss.java
@@ -1,35 +1,54 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
 /**
- * Write a description of class BlobBoss here.
+ * Boss of the game, has multiple different attacks, stronger attacks happen
+ * less, stronger attacks become harder to dodge
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Henry Ma
+ * @version January 16, 2020
  */
 public class BlobBoss extends Actor
 {
+    //Initialize variables and objects
+    //Health of boss
     private int healthPoints;
+    //Animation for boss
     private static GreenfootImage[] animation = new GreenfootImage[4];
+    //Keeps track if images for animation were created
     private static boolean createdImages = false;
+    //Frames/acts before next image switch
     private int frameRate = 8;
+    //Keeps track of image currently on
     private int imageNumber = 0;
+    //Acts before firing
     private int fireRate = 20;
+    //Acts before big attack
     private int bigAttackRate = 60;
+    //Counter for number of acts gone by
     private long animationCount = 0;
+    //Used to find player
     private ArrayList<Player> foundPlayers;
     private Actor player;
+    /**
+     * Constructor for blob boss, set and initialize values for boss
+     */
     public BlobBoss()
     {
         healthPoints = 3000;
         setImage(animation[0]);
     }
+    /**
+     * Greenfoot method, called when object is added to world
+     */
     protected void addedToWorld(World world) 
     {
         createImages();
         foundPlayers = new ArrayList<Player>(getWorld().getObjects(Player.class));
         player = foundPlayers.get(0); 
     }
-    
+    /**
+     * Act - Blob stays still while charging up for attacks/attacking
+     */
     public void act() 
     {
         animationCount ++;
@@ -58,7 +77,11 @@ public class BlobBoss extends Actor
         }
         
     }    
-    
+    /**
+     * Calls on an attack method determined by the parameters given
+     * 
+     * @param attackNumber      The attack to be called
+     */
     private void pickAttack(int attackNumber)
     {
         if(attackNumber == 0)
@@ -70,7 +93,9 @@ public class BlobBoss extends Actor
             smallAttackTwo();
         }
     }
-    
+    /**
+     * Shoots a spray of bullets(like shotgun enemy)
+     */
     private void smallAttackOne()
     {
         for(int i = 0; i < 7; i++)
@@ -78,7 +103,9 @@ public class BlobBoss extends Actor
             getWorld().addObject(new ShotgunBullet(player.getX(), player.getY() - 90 + (30 * i), 1, 6, true), getX(), getY());
         }
     }
-    
+    /**
+     * Shoots a spray of rifle bullets
+     */
     private void smallAttackTwo()
     {
         for(int i = 0; i < 3; i++)
@@ -86,7 +113,9 @@ public class BlobBoss extends Actor
             getWorld().addObject(new RifleBullet(player.getX(), player.getY() - 30 + (30 * i), 1, 10, true), getX(), getY());
         }
     }
-    
+    /**
+     * Shoots 100 bullets with an average of 25 targeted towards the player
+     */
     private void bigAttackOne()
     {
         for(int i = 0; i < 100; i++)
@@ -104,7 +133,9 @@ public class BlobBoss extends Actor
             }
         }
     }
-    
+    /**
+     * Animate by switching images
+     */
     private void animate()
     {
         if(animationCount%frameRate == 0)
@@ -113,7 +144,9 @@ public class BlobBoss extends Actor
             setImage(animation[imageNumber]);
         } 
     }
-
+    /**
+     * Create the images for the animation of this object
+     */
     private void createImages()
     {
         if(!createdImages)
@@ -125,7 +158,11 @@ public class BlobBoss extends Actor
             }
         }
     }
-    
+    /**
+     * Inflict damage dependent on the parameters given, also checks for death
+     * 
+     * @param damage    Amount of damage to deal
+     */
     public void getDamaged(int damage)
     {
         healthPoints -= damage;
@@ -134,11 +171,11 @@ public class BlobBoss extends Actor
             die();
         }
     }
-
+    /**
+     * Remove this object from the world
+     */
     private void die()
     {
         getWorld().removeObject(this);
     }
-    
-    
 }
